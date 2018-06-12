03f5f356a24430212a25eb6d92f11bb88b295f8e
mcmiro <miro.grujin@hotmail.com>
2018-06-12 16:00:12 +0200

    setup index and upload images and files

---
+++ assets/php/footer.php
@@ -0,0 +1,70 @@
+<!-- FOOTER BIG -->
+<div class="row footer-ver-2">
+            <div class="col-md-6 footer-info-content">
+                <div class="footer-big">
+                    <div class="col-md-2 footer-socials">
+                        <p>INFO</p>
+                    </div>
+                    <div class="col-md-10 footer-big-content">
+                        <ul>
+                            <b>Transfer zum Flughafen</b>
+                            <li>
+                                <a href="https://www.das-flughafentaxi-wien.at">Flughafentaxi Wien</a>
+                            </li>
+                            <li>
+                                <a href="vom-flughafen">Taxi vom Flughafen</a>
+                            </li>
+                            <li>
+                                <a href="zum-flughafen">Taxi zum Flughafen</a>
+                            </li>
+                            <li>
+                                <a href="flughafentaxi-mit-kindersitz">Flughafentaxi mit Kindersitz</a>
+                            </li>
+                            <li>
+                                <a href="bus-zug-cat">Bus - Zug - CAT</a>
+                            </li>
+                        </ul>
+
+                        <ul>
+                            <b>Info</b>
+                            <li>
+                                <a href="faq">FAQ</a>
+                            </li>
+                            <li>
+                                <a href="agb">AGB</a>
+                            </li>
+                            <li>
+                                <a href="datenschutz">Datenschutz</a>
+                            </li>
+                            <li>
+                                <a href="impressum">Impressum</a>
+                            </li>
+                        </ul>
+                        <ul>
+                            <b>Für Anbieter</b>
+                            <li>
+                                <a href="eintragen">Vorteile</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 footer-img-bg">
+                <div class="col-sm-6 footer-big-content">
+                <span>Ihr Flughafentaxi wartet schon!</span>
+                <div class="customers_button">
+                            <a href="#">Anbieter Liste</a>
+                        </div>
+                </div>
+                <div class="col-sm-6 big-footer-img"></div>
+            </div>
+        </div>
+<footer>
+           <div class="bgfooter">
+            <div class="footer-bar">
+                <div class="container">
+                    <h5>Alle Rechte vorbehalten © Das Flughafentaxi Wien <?php echo date('Y'); ?></h5>
+                </div>
+            </div>
+        </div>
+</footer>
--- index.php
+++ index.php
@@ -1,13 +1,77 @@
 <!DOCTYPE html>
-<html lang="de">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>index</title>
+    <title>Flughafentaxi Wien » Eine Seite viele Anbieter</title>
+    <meta name="description" content=""
+    />
+    <meta name="keywords" content=""/>
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- load external file to include favicons  -->
+    <?php include_once('assets/php/favico.php'); ?>
+
+
+    <!-- RS5.0 Layers and Navigation Styles -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-test
+<div class="row">
+    <div class="container">
+        <header>
+            <div class="col-md-12">
+                <div class="col-md-3 logo">
+                    <img src="assets/img/logo/logo.svg" alt="Suchmaschninenoptimierung Wien Logo">
+                </div>
+                <div class="col-md-9 navigation">
+                    <nav>
+                        <ul>
+                            <li class="active"><a href="">HEYIO</a></li>
+                            <li><a href="">Suchmaschinenoptimierung</a></li>
+                            <li><a href="">Referenzen</a></li>
+                            <li><a href="">Kontakt</a></li>
+                        </ul>
+                    </nav>
+                    <div class="side-navbar rotate-90-deg">
+                        <nav>
+                            <ul>
+                                <li class="active"><a href="">Willkommen</a></li>
+                                <li><a href="">Link</a></li>
+                                <li><a href="">Link</a></li>
+                                <li><a href="">Link</a></li>
+                                <li><a href="">Link</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="socials">
+                    <ul>
+                        <li><img src="assets/img/icons/facebook.svg" alt=""></li>
+                        <li><img src="assets/img/icons/instagram.svg" alt=""></li>
+                        <li><img src="assets/img/icons/youtube.svg" alt=""></li>
+                        <li><img src="assets/img/icons/pinterest.svg" alt=""></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 headline">
+                    <span>
+                        Hey!
+                    </span>
+                    <p>
+                        nicht in den top IO<br>
+                        dann google heyio.
+                    </p>
+                </div>
+                <div class="col-md-6"><img src="assets/img/header/chart-line.svg" alt=""></div>
+            </div>
+        </header>
+    </div>
+</div>
     
+<script src="assets/js/jquery.min.js"></script>
+<!-- <script src="assets/js/bootstrap.min.js"></script> -->
 </body>
 </html>
