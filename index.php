25a3f1dffe27837ca134dccfce04a555981a2433
mcmiro <miro.grujin@hotmail.com>
2018-06-17 19:03:30 +0200

    css edit just a base

--- index.php
+++ index.php
@@ -20,57 +20,287 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-<div class="row">
-    <div class="container">
-        <header>
-            <div class="col-md-12">
-                <div class="col-md-3 logo">
-                    <img src="assets/img/logo/logo.svg" alt="Suchmaschninenoptimierung Wien Logo">
-                </div>
-                <div class="col-md-9 navigation">
-                    <nav>
-                        <ul>
-                            <li class="active"><a href="">HEYIO</a></li>
-                            <li><a href="">Suchmaschinenoptimierung</a></li>
-                            <li><a href="">Referenzen</a></li>
-                            <li><a href="">Kontakt</a></li>
-                        </ul>
-                    </nav>
-                    <div class="side-navbar rotate-90-deg">
+    <!-- HEADER START -->
+    <div class="row">
+        <div class="container">
+            <header>
+                <div class="col-md-12">
+                    <div class="col-md-3 logo">
+                        <img src="assets/img/logo/logo.svg" alt="Suchmaschinenoptimierung Wien Logo">
+                    </div>
+                    <div class="col-md-9 navigation">
                         <nav>
                             <ul>
-                                <li class="active"><a href="">Willkommen</a></li>
-                                <li><a href="">Link</a></li>
-                                <li><a href="">Link</a></li>
-                                <li><a href="">Link</a></li>
-                                <li><a href="">Link</a></li>
+                                <li class="active"><a href="">HEYIO</a></li>
+                                <li><a href="">Suchmaschinenoptimierung</a></li>
+                                <li><a href="">Referenzen</a></li>
+                                <li><a href="">Kontakt</a></li>
                             </ul>
                         </nav>
+                        <div class="side-navbar rotate-90-deg">
+                            <nav>
+                                <ul>
+                                    <li class="active"><a href="">Willkommen</a></li>
+                                    <li><a href="">Link</a></li>
+                                    <li><a href="">Link</a></li>
+                                    <li><a href="">Link</a></li>
+                                    <li><a href="">Link</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="socials">
+                        <ul>
+                            <li><img src="assets/img/icons/facebook.svg" alt=""></li>
+                            <li><img src="assets/img/icons/instagram.svg" alt=""></li>
+                            <li><img src="assets/img/icons/youtube.svg" alt=""></li>
+                            <li><img src="assets/img/icons/pinterest.svg" alt=""></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 headline">
+                        <span>
+                            Hey!
+                        </span>
+                        <p>
+                            nicht in den top IO<br>
+                            dann google heyio.
+                        </p>
+                    </div>
+                    <div class="col-md-6"><img src="assets/img/header/chart-line.svg" alt=""></div>
+                </div>
+            </header>
+        </div>
+    </div>
+    <!-- HEADER END -->
+    <!-- WELCOME TEXT START -->
+    <div class="row welcome">
+        <div class="container">
+            <div class="headline">
+                <p>hey, hi</p>
+            </div>
+            <h1>
+                Suchmaschinenoptimierung<br>
+                ohne risiko
+            </h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet recusandae autem distinctio beatae quod dolorem reprehenderit libero nihil dolores ab quisquam accusamus officiis eius hic laboriosam id, dolor, ad vitae.</p>
+        </div>
+    </div>
+    <!-- WELCOME TEXT END -->
+    <!-- REFERENCE START -->
+    <div class="row reference">
+        <div class="container">
+            <div class="col-md-12">
+                <div class="headline">
+                    <div class="icon">
+                        <img src="assets/img/icons/chart-line-edged.svg" alt="">
+                    </div>
+                    <p>HIGH</p>
+                    <div class="path">
+                        <img src="assets/img/icons/reference.svg" alt="">
                     </div>
                 </div>
-                <div class="socials">
-                    <ul>
-                        <li><img src="assets/img/icons/facebook.svg" alt=""></li>
-                        <li><img src="assets/img/icons/instagram.svg" alt=""></li>
-                        <li><img src="assets/img/icons/youtube.svg" alt=""></li>
-                        <li><img src="assets/img/icons/pinterest.svg" alt=""></li>
-                    </ul>
-                </div>
-                <div class="col-md-6 headline">
-                    <span>
-                        Hey!
-                    </span>
-                    <p>
-                        nicht in den top IO<br>
-                        dann google heyio.
-                    </p>
-                </div>
-                <div class="col-md-6"><img src="assets/img/header/chart-line.svg" alt=""></div>
             </div>
-        </header>
+            <div class="col-md-12">
+                <div class="col-md-3 info">
+                    <div class="icon">
+                        <img src="assets/img/icons/place-stage.svg" alt="">
+                    </div>
+                    <div class="service">
+                        <p>ergebnis</p>
+                    </div>
+                    <div class="result">
+                        <p>seite 1</p>
+                    </div>
+                </div>
+                <div class="col-md-6 info active">
+                    <div class="icon">
+                        <img src="assets/img/icons/magnifying-glass.svg" alt="">
+                    </div>
+                    <div class="service">
+                        <p>Suchbegriff</p>
+                    </div>
+                    <div class="result">
+                        <p>Reinigungsfirma Wien</p>
+                    </div>
+                </div><div class="col-md-3 info">
+                    <div class="icon">
+                        <img src="assets/img/icons/timer.svg" alt="">
+                    </div>
+                    <div class="service">
+                        <p>Dauer</p>
+                    </div>
+                    <div class="result">
+                        <p>Monate 6</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="carousel">
+                <div class="col-xs-3 inactive-left">
+                    <img src="assets/img/customers/charlys.png" alt="">
+                </div>
+                <div class="col-xs-3 active">
+                    <img src="assets/img/customers/complete.png" alt="">
+                </div>
+                <div class="col-xs-3 inactive-right">
+                    <img src="assets/img/customers/fluxi.png" alt="">
+                </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-    
+    <!-- REFERENCE END -->
+    <!-- ABOUT START -->
+    <div class="row about">
+        <div class="container">
+            <div class="col-md-12">
+                <div class="col-md-7">
+                    <img src="assets/img/all/about.jpg" alt="">
+                </div>
+                <div class="col-md-5">
+                    <img src="assets/img/icons/about.svg" alt="">
+                    <div class="about-content">
+                        <div class="icon">
+                        <img src="assets/img/icons/chart-line-round.svg" alt="">
+                        </div>
+                        <div class="headline">über heyio</div>
+                        <b>WE DO STUFF LIKE PROS</b>
+                        <span>SEO MASTER</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel illum iure quis voluptate nostrum fugiat culpa minima recusandae quia praesentium. Soluta libero ratione enim, voluptatum voluptate omnis laudantium nihil et!</p>
+                        <button>MEHR</button>
+                        <div class="rotate-270-deg">
+                            <p>wir schauen nicht nach vorne</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ABOUT END -->
+    <!-- ROADMAP START -->
+    <div class="row roadmap">
+        <div class="container">
+            <div class="icon">
+                <img src="assets/img/icons/roadmap.svg" alt="">
+            </div>
+            <div class="col-md-12">
+                <div class="col-md-6 roadmap-left">
+                    <div class="col-md-9">
+                        <span>ANALYSE UND STRATEGIE</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit vitae delectus, quae, beatae velit aliquid dolorem officia ipsa deleniti explicabo natus. Architecto vero possimus ad? Dolorum vitae voluptates expedita explicabo?</p>
+                        <button>MHER DAZU</button>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="headline">
+                        <span>ANFRAGE</span>
+                        <p>Dauer...</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 roadmap-right">
+                    <div class="col-md-3">
+                        <div class="headline">
+                        <span>ANFANG</span>
+                        <p>Lorem...</p>
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <span>ANALYSE UND STRATEGIE</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit vitae delectus, quae, beatae velit aliquid dolorem officia ipsa deleniti explicabo natus. Architecto vero possimus ad? Dolorum vitae voluptates expedita explicabo?</p>
+                        <button>MHER DAZU</button>
+                    </div>
+                </div>
+                <div class="col-md-6 roadmap-left">
+                    <div class="col-md-9">
+                        <span>ANALYSE UND STRATEGIE</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit vitae delectus, quae, beatae velit aliquid dolorem officia ipsa deleniti explicabo natus. Architecto vero possimus ad? Dolorum vitae voluptates expedita explicabo?</p>
+                        <button>MHER DAZU</button>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="headline">
+                        <span>ANFRAGE</span>
+                        <p>Dauer...</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 roadmap-right">
+                    <div class="col-md-3">
+                        <div class="headline">
+                        <span>ANFANG</span>
+                        <p>Lorem...</p>
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <span>ANALYSE UND STRATEGIE</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit vitae delectus, quae, beatae velit aliquid dolorem officia ipsa deleniti explicabo natus. Architecto vero possimus ad? Dolorum vitae voluptates expedita explicabo?</p>
+                        <button>MHER DAZU</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ROADMAP END -->
+    <!-- FOOTER START -->
+    <footer>
+        <!-- IMPRESSUM START -->
+        <div class="row impressum">
+            <div class="container">
+                <div class="icon">
+                    <img src="assets/img/icons/impressum.svg" alt="">
+                </div>
+                <div class="col-md-12">
+                    <div class="col-md-4 info">
+                        <img src="assets/img/icons/telefon.svg" alt="">
+                        <span>David<br>
+                        Radanovic<br>
+                        e.U.</span>
+                        <p>
+                        1160 Wien, Feßtgasse 6/8-11<br>
+                        UID: ATU123456
+                        </p>
+                        <div class="bottom-text">
+                            <a href="tel:+436643925387">0664/392 53 87</a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 sitemap">
+                    <img src="assets/img/icons/sitemap.svg" alt="">
+                    <ul>
+                        <li>
+                            <a href="suchmaschinenoptimierung">Suchmaschinenoptimierung</a>
+                            <a href="index">Heyio</a>
+                            <a href="leistungen">Leistungen</a>
+                            <a href="Kontakt">kontakt</a>
+                            <a href="agb">AGB's</a>
+                            <a href="datenschutz">Datenschutz</a>
+                        </li>
+                    </ul>
+                    <div class="bottom-text">
+                            <p>QUICK LINKS</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 contact">
+                        <img src="assets/img/icons/letter.svg" alt="">
+                        <form action="mailer/kontakt">
+                            <input type="text" placeholder="NAME">
+                            <input type="email" placeholder="E_MAIL">
+                            <textarea name="nachricht" id="nachricht" cols="30" rows="10" placeholder="NACHRICHT">
+                            </textarea>
+                            <input type="submit" value="Senden">
+                        </form>
+                        <div class="bottom-text">
+                                <p>QUICK LINKS</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- IMPRESSUM END -->
+        <div class="container copyright">
+        <p>&copy;<?=date('Y'); ?> Heyio Web Agency</p>
+        </div>
+    </footer>
+    <!-- FOOTER END -->
 <script src="assets/js/jquery.min.js"></script>
 <!-- <script src="assets/js/bootstrap.min.js"></script> -->
 </body>
